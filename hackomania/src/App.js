2bf67ec1717ca17ae43a0f2c3a063af3a5c0f69e
TrevorToh <145330041+TrevorToh@users.noreply.github.com>
2025-02-15 16:00:57 +0800

    Added header
    skibidi dop dop dop yes yes skibidi dop dop dee dee

--- hackomania/src/App.js
+++ hackomania/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [recents,setRecents] = useState(["Test", "Test", "Test","Test", "Test", "Test", "Test", "Test"])
+
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a style={{padding: 30, fontWeight:'bold', fontSize:30}}>        Kitchen Copilot</a>
+
+        <div className='recents'>
+            <p>Recents: </p>
+
+            {recents.map((recent)=>{
+            return(
+              <p style={{marginLeft: 5,}}> 
+                {recent}
+              </p>
+            )
+          })}
+
+        </div>
       </header>
+      <div className="body">
+      </div>
     </div>
   );
 }

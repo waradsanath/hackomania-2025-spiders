25f388c9bf88ee156d74e8157d009b90404e8346
TrevorToh <145330041+TrevorToh@users.noreply.github.com>
2025-02-16 12:20:44 +0800

    A

--- hackomania/src/testingGemini.js
+++ hackomania/src/testingGemini.js
@@ -1,27 +1,36 @@
 import { use, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Link } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
 
 function Geminie() {
     const [prompta,setPrompta] = useState('')
-    const [reply,setReply] = useState("type your prompt and press send!")
-    const [chatHistory, setChatHistory] = useState("")
-    const navigate = useNavigate()
+    const [ingredientsa,setIngredients] = useState("")
+    const [answer,setAnswer] = useState("type your prompt and press send!")
+
     const genAI = new GoogleGenerativeAI("AIzaSyCjYD-Dh4i7tjiLXss7hxYl9hnQ97YBYV4");
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const sendMesssage = async () => {
         try {
         //call gemini
-        setReply(null)
+        
         console.log("Loading...")
-        const result = await model.generateContent("make a step by step recipie for making" + prompta)
+        const ingredients = await model.generateContent("Write a list of ingredients for making" + prompta+ ", in the following format: \n Ingredients: A, B, C. Strictly do not bold any words ")
+        
+
         console.log("still loading...")
-        const response = await result.response;
+        const responseIngredients = await ingredients.response;
+        setIngredients(responseIngredients.candidates[0].content.parts[0].text)
+        console.log(ingredientsa)
+
+        const recipe = await model.generateContent("Using the" +ingredientsa +", make a recipe for "+prompta+". Strictly do not bold any words and do not write the ingredients")
+
+        const responseRecipe = await recipe.response;
         console.log("Finished!")
-        console.log(response)
-        setReply(response.candidates[0].content.parts[0].text)
+        console.log(responseIngredients)
+
+        
+        setAnswer(responseRecipe.candidates[0].content.parts[0].text)
 
 
         
@@ -36,24 +45,24 @@ function Geminie() {
         <div className="App">
         <header className="App-header">
             <a style={{padding: 15, fontWeight:'bold', fontSize:30}}>        Kitchen Copilot</a>
-            <header className="App-header">
-            <a style={{padding: 30, fontWeight:'bold', fontSize:30}}>        Kitchen Copilot</a>
-            <button onClick={()=>{navigate("/")}} className="logut">Home</button>
-            <button onClick={()=>{navigate("/Gemini")}} className="logut">Generate Recipies</button>
-            <button onClick={()=>{navigate("/Login")}} className="logout">Log Out</button>
-      </header>
+
             <div className='recents'>
                 <Link to="/">To Home</Link>
             </div>
         </header>
-<div className="body">
+        <div className="body">
             <div className="gemini">
-                <div style={{padding:100, fontSize:50}}> 
-                <input placeholder="value" type="text" onChange={(e)=>setPrompta(e.target.value)}/>
-                <button onClick={sendMesssage}>Send</button>
+
+                <div className="gemininput"> 
+                    <input placeholder="type what you want to make here!" type="text" onChange={(e)=>setPrompta(e.target.value)} className="gemininput"/>
+                    <button onClick={sendMesssage}>Find recipe</button>
                 </div>
                 
-                {reply ? <p>{reply}</p> : <p>Loading...</p>}
+                {ingredientsa ? 
+                <div>
+                    <b>{ingredientsa}</b>
+                    <p>{answer}</p>
+                </div> : <p>Loading...</p>}
 
                 
                 </div>
